61719ac54ef94082a6bd58ac8d61cad6d3c29dd3
Marwin Wiechert <marwinwiechert@icloud.com>
2018-01-20 10:09:23 +0100

    Initial Commit

---
+++ RockPaperScissors/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RockPaperScissors
+//
+//  Created by Marwin Wiechert on 20.01.18.
+//  Copyright © 2018 Marwin Wiechert. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
